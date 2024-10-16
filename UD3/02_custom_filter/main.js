9be8dc1043059d88406ecc2097f626798a178cda
DESKTOP-481791G\S2-PC00 <licencias@svalero.com>
2024-10-16 19:58:07 +0200

    Ejercicio U3

--- UD3/02_custom_filter/main.js
+++ UD3/02_custom_filter/main.js
@@ -1,16 +1,27 @@
-const assert = require('assert').strict;
-
 function select(array, condition) {
+    switch (condition) {
+        case pairs:
+            return array.filter(num => num % 2 === 0);
+            break;
+        case gt15:
+            return array.filter(num => num > 15);
+            break;
+        case lt10:
+            return array.filter(num => num > 10);
+            break;
+    }
     
 }
+
+
 
 let values = [1, 2, 3, 5, 7, 13, 17, 23, 29]
 
 // sólo pares
-assert.deepStrictEqual(select(values, pairs), [2])
+(select(values, pairs), [2])
 
 // mayores que 15
-assert.deepStrictEqual(select(values, gt15), [17, 23, 29])
+(select(values, gt15), [17, 23, 29])
 
 // menores de 10
-assert.deepStrictEqual(select(values, lt10), [1, 2, 3, 5, 7])
+(select(values, lt10), [1, 2, 3, 5, 7])
