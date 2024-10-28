72e63e39d0d6278224bd585e87a9874d64ab13eb
DESKTOP-481791G\S2-PC00 <licencias@svalero.com>
2024-10-28 17:04:29 +0100

    Ejercicio 5 y 6

--- UD3/05_dom/main.js
+++ UD3/05_dom/main.js
@@ -2,3 +2,12 @@ let listarDivs = document.querySelectorAll('div')
 listarDivs.forEach(div => {
     console.log(div)
 })
+
+let listarButtons = document.getElementsByClassName('buttons');
+console.log(listarButtons)
+
+let listarDivClassButtons = document.querySelector('.buttons')
+console.log(listarDivClassButtons)
+
+let todosDivsButtons = document.querySelectorAll('div.buttons')
+console.log(todosDivsButtons)
--- UD3/06_modify_dom/exercise01/main.js
+++ UD3/06_modify_dom/exercise01/main.js
@@ -1,5 +1,27 @@
 window.onload = function(e) {
-    console.log('documento cargado')
+    
+    //1
+    document.getElementsByTagName('h2')[1].innerText = 'Texto cambiado!'
+    
+    //2
+    document.getElementById('username')
+    
+    //3
+    let colorChangeFirst = document.querySelectorAll('article .first')
+    for (let item of colorChangeFirst) {
+        item.style.color = 'red'
+    }
 
-    // document.getElementsByTagName('h1')[0].innerText = 'Changed from JS!!'
+    //4
+    document.querySelectorAll('li .item')
+
+    //5
+    const allButtons = document.querySelectorAll('.buttons button')
+    for (let button of allButtons) {
+        button.style.color = 'blue'
+    }
+
+    //6
+    const primerParrafo = document.querySelectorAll('p')[0]
+    primerParrafo.style['background-color'] = 'pink'
 }
